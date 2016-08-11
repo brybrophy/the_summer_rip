5653918755f15dd17823a2d86b25275ee83bf43f
John Carney <jcarney000@gmail.com>
2016-08-10 20:09:46 -0700

    add more pants logic

--- index.js
+++ index.js
@@ -3,15 +3,30 @@
 (function(){
   const $lowLeftLeg = $('#lowLeftLeg');
   const $lowRightLeg = $('#lowRightLeg');
+  let pantsLeft = 2;
 
   $lowLeftLeg.click(() => {
     $lowLeftLeg.fadeTo(500, 0.01)
-    
+    pantsLeft -= 1;
+    if (pantsLeft === 0) {
+      pantsOff()
+    }
   });
 
   $lowRightLeg.click(() => {
     $lowRightLeg.fadeTo(500, 0.01)
+    pantsLeft -= 1;
+    if (pantsLeft === 0) {
+      pantsOff();
+    }
   });
+
+  const pantsOff = function() {
+    $('#sunglasses').fadeIn('fast');
+    console.log('done')
+    // sunglasses fade in
+    //
+  }
 
 
 })();
